215b168903721168e94d7a98ab396916f19cf585
Fabu András <fabuandras@gmail.com>
2024-04-15 10:26:21 +0200

    oraiMunka

---
+++ src/oraimunka/OraiMunka.java
@@ -0,0 +1,20 @@
+package oraimunka;
+
+public class OraiMunka {
+
+    public static void main(String[] args) {
+        Redony elso = new Redony();
+        elso.nev = "Bal oldali";
+        elso.szin = "Szürke1";
+        elso.sorszam = 1;
+        Redony masodik = new Redony();
+        masodik.nev = "Középső";
+        masodik.szin = "Szürke2";
+        masodik.sorszam = 2;
+        Redony harmadik = new Redony();
+        harmadik.nev = "Jobb oldali";
+        harmadik.szin = "Szürke3";
+        harmadik.sorszam = 3;
+    }
+    
+}
---
+++ src/oraimunka/Redony.java
@@ -0,0 +1,9 @@
+package oraimunka;
+
+public class Redony {
+    int sorszam;
+    String nev, szin;
+    void inditas () {
+        System.out.println("redőny indítás");
+    }
+}
